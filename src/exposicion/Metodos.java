8fb21b0fdb41707c459f02e927eb0019727801ba
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2017-02-23 09:11:06 +0100

    fin

--- src/exposicion/Metodos.java
+++ src/exposicion/Metodos.java
@@ -13,44 +13,62 @@ import javax.swing.JOptionPane;
  * @author acomesanavila
  */
 public class Metodos {
-    ArrayList<Carta> jugador1= new ArrayList();
-    ArrayList<Carta> jugador2= new ArrayList();
-   public void cargarArray(){
-       jugador1.add(new Carta(2,"bastos"));
-   }
-    public void pares(){
-        for(int i=0;i<jugador1.size();i++)
-        if(jugador1.equals(jugador1.get(i)))
-            jugador1.remove(i);
+
+    ArrayList<Carta> baraja = new ArrayList();//llenar con todas las cartas
+    ArrayList<Carta> jugador1;
+    ArrayList<Carta> jugador2;
+
+    public void barajar() {//con random
+
     }
-        public void pares2(){
-        for(int i=0;i<jugador2.size();i++)
-        if(jugador2.equals(jugador2.get(i)))
-            jugador2.remove(i);
+
+    public void cargarArray() {//se llenan los array de cada jugador con una porcion del array baraja
+        jugador1 = new ArrayList(baraja.subList(0, 0));// 0= posicion inicial y final en el array baraja
+        jugador2 = new ArrayList(baraja.subList(0, 0));
     }
-     public void robar(){
-         Object num=Integer.parseInt(JOptionPane.showInputDialog("Numero carta"));
-         jugador1.add((Carta) num);
-         jugador2.remove(num); 
-         pares();
-     }   
-   public void robar2(){
-         Object num=Integer.parseInt(JOptionPane.showInputDialog("Numero carta"));
-         jugador2.add((Carta) num);
-         jugador1.remove(num);  
-         pares2();
-     }   
-   public void ganador(){
-       if(jugador1.isEmpty()==true)
-           System.out.println("Jugador1 gana");
-       else{
-           System.out.println("jugador 2 gana");
-       }
-   }
-   public void turnos(){
-       do{
-           robar();
-           robar2();
-       }while(jugador1.isEmpty()==true||jugador2.isEmpty()==true);       
-   }
+
+    public void pares() {
+        for (int i = 0; i < jugador1.size(); i++) {
+            if (jugador1.equals(jugador1.get(i))) {
+                jugador1.remove(i);
+            }
+        }
+    }
+
+    public void pares2() {
+        for (int i = 0; i < jugador2.size(); i++) {
+            if (jugador2.equals(jugador2.get(i))) {
+                jugador2.remove(i);
+            }
+        }
+    }
+
+    public void robar() {
+        Object num = Integer.parseInt(JOptionPane.showInputDialog("Numero carta"));
+        jugador1.add((Carta) num);
+        jugador2.remove(num);
+        pares();
+    }
+
+    public void robar2() {
+        Object num = Integer.parseInt(JOptionPane.showInputDialog("Numero carta"));
+        jugador2.add((Carta) num);
+        jugador1.remove(num);
+        pares2();
+    }
+
+    public void ganador() {
+        if (jugador1.isEmpty() == true) {
+            System.out.println("Jugador1 gana");
+        } else {
+            System.out.println("jugador 2 gana");
+        }
+    }
+
+    public void turnos() {
+        do {
+            robar();
+            robar2();
+        } while (jugador1.isEmpty() == true || jugador2.isEmpty() == true);
+    }
 }
