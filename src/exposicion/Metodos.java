b49d3534d7bb85629178b3c2846ce84b371abf12
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2017-02-23 09:29:05 +0100

    fin

--- src/exposicion/Metodos.java
+++ src/exposicion/Metodos.java
@@ -6,6 +6,7 @@
 package exposicion;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import javax.swing.JOptionPane;
 
 /**
@@ -13,20 +14,38 @@ import javax.swing.JOptionPane;
  * @author acomesanavila
  */
 public class Metodos {
-    
+
     ArrayList<Carta> baraja = new ArrayList();//llenar con todas las cartas
     ArrayList<Carta> jugador1;
     ArrayList<Carta> jugador2;
-    
-    public void barajar() {//con random
 
+    public void barajar() {
+        Collections.shuffle(baraja);//desordena el array
     }
-    
+
+    public void amosar() {
+        for (int i = 0; i < baraja.size(); i++) {
+            System.out.println(baraja.get(i));//Comprobacion del orden de la baraja
+        }
+    }
+
+    public void amosarManoJugador1() {
+        for (int i = 0; i < jugador1.size(); i++) {
+            System.out.println(jugador1.get(i));//Mano del jugador 1
+        }
+    }
+
+    public void amosarManoJugador2() {
+        for (int i = 0; i < jugador2.size(); i++) {
+            System.out.println(jugador2.get(i));//Mano del jugador 2
+        }
+    }
+
     public void cargarArray() {//se llenan los array de cada jugador con una porcion del array baraja
         jugador1 = new ArrayList(baraja.subList(0, 0));// 0= posicion inicial y final en el array baraja
         jugador2 = new ArrayList(baraja.subList(0, 0));
     }
-    
+
     public void pares() {
         Object aux = null;
         for (int j = 0; j < jugador1.size(); j++) {//carta a comparar
@@ -38,7 +57,7 @@ public class Metodos {
             }
         }
     }
-    
+
     public void pares2() {
         Object aux = null;
         for (int j = 0; j < jugador2.size(); j++) {//carta a comparar
@@ -50,21 +69,21 @@ public class Metodos {
             }
         }
     }
-    
+
     public void robar() {
         int num = Integer.parseInt(JOptionPane.showInputDialog("Numero carta"));//elige posicion de la carta de la mano contraria
         jugador1.add(jugador2.get(num - 1));// añade a tu mano la carta elegida 
         jugador2.remove(num - 1);// elimina la carta en la mano contraria
         pares();//realiza pares para comprobar si hay una pareja 
     }
-    
+
     public void robar2() {
         int num = Integer.parseInt(JOptionPane.showInputDialog("Numero carta"));
         jugador2.add(jugador1.get(num));
         jugador1.remove(num);
         pares2();
     }
-    
+
     public void ganador() {
         if (jugador1.isEmpty() == true) {
             System.out.println("Jugador1 gana");
@@ -72,7 +91,7 @@ public class Metodos {
             System.out.println("jugador 2 gana");
         }
     }
-    
+
     public void turnos() {
         do {
             robar();
